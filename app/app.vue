33784fbdea4714cc350b70990dc4cbbac763cf92
Vin Hegewald <vh@agital.online>
2025-09-05 10:18:22 +0200

    feat: add tailwindcwss

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl bg-red-500">Schul-Projekt</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image'
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss'
   ]
 })
