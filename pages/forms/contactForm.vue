a0248acd62c73a3d6192b0a374e5ff96b2f28a9e
Diana Muhammetnazarova <diana.h.muhammet@gmail.com>
2025-11-28 09:41:18 +0100

    Contact Form:

---
+++ pages/forms/contactForm.vue
@@ -0,0 +1,20 @@
+<!--<script setup lang="ts">-->
+<script>
+import ContactForm from '~/components/forms/contactForm.vue';
+
+export default {
+  name: 'ContactFormPage',
+  components: {
+    ContactForm,
+  },
+};
+</script>
+<template>
+  <div>
+    <h1>Kontakt Formuler</h1>
+    <ContactForm />
+  </div>
+</template>
+<style scoped>
+</style>
+
